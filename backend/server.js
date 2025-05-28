99edf9303b919a2acc6afb13cd7520e522dd6f98
JCFLuxTalent <mogroupe@gmail.com>
2025-05-28 18:02:56 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const PORT = process.env.PORT || 3000;
+
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
@@ -21,7 +22,6 @@ app.get('/api/disponibilites', (req, res) => {
   ]);
 });
 
-const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Serveur lancé sur le port ${PORT}`);
 });
