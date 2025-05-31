ad95d9c66827e5cbb26401c76d5312fe26907a6e
JCFLuxTalent <mogroupe@gmail.com>
2025-05-31 18:59:27 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,26 +1,23 @@
 const express = require('express');
 const cors = require('cors');
-const db = require('./db');
-const fs = require('fs');
 const path = require('path');
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Chargement et exécution du script SQL à chaque démarrage
-const initSql = fs.readFileSync(path.join(__dirname, 'init.sql')).toString();
-db.query(initSql)
-  .then(() => console.log('✅ Tables créées ou déjà existantes.'))
-  .catch((err) => console.error('❌ Erreur création des tables :', err));
+app.post('/api/reservations', (req, res) => {
+    console.log('Nouvelle réservation reçue :', req.body);
+    res.send('Réservation bien reçue, merci !');
+});
 
-// Route de test
 app.get('/', (req, res) => {
-  res.send('API JCF Lux Talent en ligne');
+    res.send('API JCF Lux Talent en ligne');
 });
 
 app.listen(PORT, () => {
-  console.log(`Serveur lancé sur le port ${PORT}`);
+    console.log(`Serveur lancé sur le port ${PORT}`);
 });
